522702cb3cacc51536fd6ff54c5dfd309ba87584
PhyaeSoneThwim <pyaesonethwim1998@gmail.com>
2020-12-10 22:29:12 +0630

    react canvas svg map example

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import Map from "./Map";
+import { MAP } from "./Map/constants";
+
+import "./styles.css";
+
+function App() {
+	return (
+		<div className="App">
+			<Map
+				data={{
+					map: MAP,
+					width: 600,
+					height: 500,
+					center: [-70, -38],
+					scale: 380,
+					currency: "CLP",
+				}}
+			/>
+		</div>
+	);
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
